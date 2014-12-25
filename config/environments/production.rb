f6c094a7335f583b96ed4c723535f1addf0f3cb4
shakyeah <dshakya@Darshans-MacBook-Pro.local>
2014-12-25 17:26:22 -0500

    add config for development/production/test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # config/environments/test.rb
+  config.ember.variant = :production
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
